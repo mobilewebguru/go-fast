19b6f8040ae885cc326917bfc6235e5513cbf31d
iosandroidwebtopdev <strong.iron.90@gmail.com>
2018-07-23 14:25:02 +1000

    add build

---
+++ utilities/mailUtil_test.go
@@ -0,0 +1,24 @@
+package utilities
+
+/*
+func TestSendTemplate(t *testing.T) {
+	// Only pass t into top-level Convey calls
+	Convey("Given I want to send mail-verify message to user", t, func() {
+		configUtil := mocks.ConfigUtilMock{}
+		mailUtil := NewMailUtil(configUtil)
+		Convey("When send", func() {
+			// send verify email
+			userName := "ayman"
+			globalVars := map[string]interface{}{"FNAME": userName, "ACTIVATE_ACCOUNT": "google.com/verify/registration?token=testToken&email=test@mail.com"}
+
+			_, mailErr := mailUtil.SendTemplate("enkhalifapro@gmail.com", "enkhalifapro@gmail.com", "John Curtis", "Verify your e-mail", globalVars, 606061)
+			if mailErr != nil {
+				panic(mailErr)
+			}
+			Convey("error should be nil", func() {
+				So(mailErr, ShouldEqual, nil)
+			})
+		})
+	})
+}
+*/
